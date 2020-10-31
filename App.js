abe4d2f28c9014d5f4da711faa58e644071a7e58
Vitor <vitorbarbosa1998@hotmail.com>
2020-10-31 18:25:51 +0000

    feat: added routes and vavigation

--- App.js
+++ App.js
@@ -1,13 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
+
+const Routes = createAppContainer(
+  createSwitchNavigator({
+    Login,
+    List,
+    Book
+  })
+);
+function Login(params) {
+  return(<View style={styles.container}>
+    <Text>Login</Text>
+    <StatusBar style="auto" />
+  </View>)
+}
+function List(params) {
+  return(<View style={styles.container}>
+    <Text>List</Text>
+    <StatusBar style="auto" />
+  </View>)
+}
+function Book(params) {
+  return(<View style={styles.container}>
+    <Text>Book</Text>
+    <StatusBar style="auto" />
+  </View>)
+}
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    
+    <Routes></Routes>
   );
 }
 
